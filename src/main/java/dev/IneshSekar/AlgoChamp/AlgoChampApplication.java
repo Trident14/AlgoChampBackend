73c3dba6311e4077e44004bde0f038af05f1ab80
Trident14 <sekarinesh14@gmail.com>
2024-07-01 15:35:20 +0530

    config env variable

--- src/main/java/dev/IneshSekar/AlgoChamp/AlgoChampApplication.java
+++ src/main/java/dev/IneshSekar/AlgoChamp/AlgoChampApplication.java
@@ -3,12 +3,11 @@ package dev.IneshSekar.AlgoChamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class AlgoChampApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AlgoChampApplication.class, args);
 	}
 
